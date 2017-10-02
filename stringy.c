0bd28bca22fae15a86028c1297fe19edc53d03af
iwang2 <iwang2@stuy.edu>
2017-10-02 10:59:18 -0400

    strncmp - untested

--- stringy.c
+++ stringy.c
@@ -6,7 +6,21 @@
 
 //strlen
 
-//strcmp
+//strncmp
+int mycmpn(char *s1, char *s2, int n){
+  int length = strlen(s1);
+  int i = length;
+  if(strlen(s2) < length){
+    length = strlen(s2);
+  }
+  if(length > n){
+    length = n;
+  }
+  while(s1[length - i] == s2[length - i]){
+    i++;
+  }
+  return s1[length - i] - s1[length - i];
+}
 
 //strchr
 
