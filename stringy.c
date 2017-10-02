3bbd47f687edb24915b528e6a1d568fa29a29038
Mansour <melsharawy@stuy.edu>
2017-10-02 10:40:19 -0400

    main method and strchr WIP

--- stringy.c
+++ stringy.c
@@ -9,7 +9,23 @@
 //strcmp
 
 //strchr
-
+char *mystrchr(char *s, char *c){
+  char *ret;
+  int i = 0;
+  while(s[i]){
+    if(s[1] == *c){
+      *ret = s + i;
+    }
+  }
+  return ret;
+}
 //strcat
 
 //strstr (to be extra)
+
+
+int main(){
+
+
+  return 0;
+}
